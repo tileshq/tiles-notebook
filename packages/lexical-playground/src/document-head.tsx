45647a6d2854303812739090df1be99f2d08f026
Ankesh Bharti <hey@ankeshbharti.com>
2025-04-02 20:04:37 +0700

    Update favicon to use SVG format in lexical playground

--- packages/lexical-playground/src/document-head.tsx
+++ packages/lexical-playground/src/document-head.tsx
@@ -16,11 +16,13 @@ export default function DocumentHead(): null {
     // Set favicon
     const favicon = document.querySelector("link[rel='icon']") as HTMLLinkElement;
     if (favicon) {
-      favicon.href = '/favicon.ico';
+      favicon.href = '/src/images/logo.svg';
+      favicon.type = 'image/svg+xml';
     } else {
       const newFavicon = document.createElement('link');
       newFavicon.rel = 'icon';
-      newFavicon.href = '/favicon.ico';
+      newFavicon.type = 'image/svg+xml';
+      newFavicon.href = '/src/images/logo.svg';
       document.head.appendChild(newFavicon);
     }
   }, []);
